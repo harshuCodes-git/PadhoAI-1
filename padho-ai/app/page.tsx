26a61f34e9a0d8c4684e0b3b4f42da755d2cb563
harshuCodes-git <1032210414@tcetmumbai.in>
2024-06-21 02:12:40 +0530

    authentication is Completed

---
+++ padho-ai/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { TracingBeam } from "../components/ui/tracing-beam";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <>
+    <div>this is page.tsx</div>
+    
+      {/* <Image src="/images/hero-image.jpg" alt="hero image" width={1920} height={1080} /> */}
+    <Button/>
+    </>
+  );
+}
